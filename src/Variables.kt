03d011a588b9c9e790845421e0df67ed9f46e10d
unknown <ingvalles97@gmail.com>
2024-05-26 08:58:43 -0600

    Variables
    Curso del youtube

---
+++ src/Variables.kt
@@ -0,0 +1,35 @@
+fun main() {
+    //int
+    val age:Int = 30
+
+    //Long
+    val example:Long = 300000000
+
+    //Float
+    val floatExample:Float = 30.31F
+
+    //Double
+    val doubleExample:Double = 30.34654321
+
+    /**
+     * Variables Alphanumerical
+     */
+
+    //Char
+    val charExample1:Char = 'e'
+    val charExample2:Char = '2'
+    val charExample3:Char = '@'
+
+    //String
+    val stringExample:String = "Simona la mona pelona"
+
+    /**
+     * Variables Boolean's
+     */
+
+    val booleanExample:Boolean = true
+    val booleanExample2:Boolean = false
+    val booleanExample3:Boolean = false
+
+    print(stringExample)
+}
